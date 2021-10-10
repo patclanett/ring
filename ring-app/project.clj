08ea83ee36020b56483ad3dec96a7773d422604c
tommy <tommy.brumley95@gmail.com>
2021-10-10 12:43:20 -0700

    first commit

---
+++ ring-app/project.clj
@@ -0,0 +1,12 @@
+(defproject ring-app "0.1.0-SNAPSHOT"
+  :description ""
+  :url ""
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.2"]
+                 [metosin/ring-http-response "0.9.1"]
+                 [metosin/muuntaja "0.6.7"]
+                 [metosin/reitit "0.5.11"]]
+  :repl-options {:init-ns ring-app.core}
+  :main ring-app.core)
---
+++ ring-app/src/ring_app/core.clj
@@ -0,0 +1,67 @@
+(ns ring-app.core
+  (:require
+    [reitit.ring :as reitit]
+    [ring.adapter.jetty :as jetty]
+    [ring.util.http-response :as response]
+    [ring.middleware.reload :refer [wrap-reload]]
+    [muuntaja.middleware :as muuntaja]))
+
+
+
+(defn html-handler [request-map]
+  (response/ok
+    (str "<html><body> your IP is: "
+      (:remote-addr request-map)
+      "</body></html>")))
+
+(defn json-handler [request]
+  (response/ok
+    {:result (get-in request [:body-params :id])}))
+
+(def handler json-handler)
+
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
+(def routes
+  [["/" html-handler]
+   ["/echo/:id"
+    {:get
+      (fn [{{:keys [id]} :path-params}]
+        (response/ok (str "<p>The value is: " id "</p>")))}]
+   ["/api" {:middleware [wrap-formats]}
+      ["/multiply"
+       {:post
+         (fn [{{:keys [a b]} :body-params}]
+           (response/ok {:result (* a b)}))}]]])
+
+
+
+(def handler
+  (reitit/ring-handler
+    (reitit/router routes)
+    (reitit/routes
+      (reitit/create-resource-handler {:path "/"})
+      (reitit/create-default-handler
+        {:not-found
+          (constantly (response/not-found "404 - Page not found"))
+         :method-not-allowed
+          (constantly (response/method-not-allowed "405 - Not allowed"))
+         :not-acceptable
+          (constantly (response/not-acceptable "406 - Not acceptable"))}))))
+
+
+(defn -main []
+  (jetty/run-jetty
+    (-> #'handler
+        wrap-nocache
+        wrap-reload)
+    {:port 3000
+     :join? false}))
